5d8ff447daa3547c3f125389e45a55afe57a91e6
Luciano Tavernard <lucianotavernard@gmail.com>
2022-08-11 00:44:07 -0300

    chore: adjust initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import favicon from './assets/favicon.svg'
+
 export function App() {
-  return <h1>ViteJS</h1>
+  return (
+    <main className="text-center">
+      <section className="flex flex-col justify-center items-center h-screen bg-[#282c34] text-white">
+        <img
+          alt="logo"
+          src={favicon}
+          className="h-[40vmin] pointer-events-none animate-spin-slow"
+        />
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+
+        <a
+          className="text-[#646cff] hover:underline"
+          target="_blank"
+          href="https://vitejs.dev"
+          rel="noopener noreferrer"
+        >
+          Learn Vite
+        </a>
+      </section>
+    </main>
+  )
 }
